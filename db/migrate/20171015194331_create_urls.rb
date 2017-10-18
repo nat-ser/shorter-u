ffe00da5e5e0bc37e0e91acfb820e083b3c1ba3e
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2017-10-17 20:39:22 -0400

    why was i defaulting count to 0??

--- db/migrate/20171015194331_create_urls.rb
+++ db/migrate/20171015194331_create_urls.rb
@@ -2,7 +2,7 @@ class CreateUrls < ActiveRecord::Migration[5.0]
   def change
     create_table :urls do |t|
       t.string :full_address, null: false, default: :null
-      t.integer :visit_count, default: 0
+      t.integer :visit_count, default: 1
 
       t.timestamps
     end
