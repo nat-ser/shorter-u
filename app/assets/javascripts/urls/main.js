065ae02ce8e1d9dc606cf029b064add2c058bbe9
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2017-10-18 01:54:15 -0400

    discombobulated styling

--- app/assets/javascripts/urls/main.js
+++ app/assets/javascripts/urls/main.js
@@ -1,8 +1,8 @@
 $(function() {
   function bindAjaxSubmit() {
     $("#new_url").on("ajax:success", function(event, data) {
-      $("#error-msg").hide();
-      $(".short-link").html(data.url).css("display", "block");
+      $("#error-msg").css("display", "none")
+      $(".short-link").html(data.url).slideDown();
     }).on("ajax:error", function(event, xhr, status) {
       var error = $.parseJSON(xhr.responseText).error
       displayErrors(error);
@@ -10,8 +10,8 @@ $(function() {
   }
 
   function displayErrors(error) {
-    $(".short-link").css("display", "none")
-    $("#error-msg").html(error);
+    $(".short-link").fadeOut();
+    $("#error-msg").html(error).css("display", "block")
   }
 
   bindAjaxSubmit();
