44f5f7a8824a23656990588b0ca364ce2d0ef750
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2017-10-18 10:27:31 -0400

    more styling

--- app/assets/javascripts/urls/main.js
+++ app/assets/javascripts/urls/main.js
@@ -2,7 +2,7 @@ $(function() {
   function bindAjaxSubmit() {
     $("#new_url").on("ajax:success", function(event, data) {
       $("#error-msg").css("display", "none")
-      $(".short-link").html(data.url).slideDown();
+      $(".short-link").val(data.url).slideDown();
     }).on("ajax:error", function(event, xhr, status) {
       var error = $.parseJSON(xhr.responseText).error
       displayErrors(error);
